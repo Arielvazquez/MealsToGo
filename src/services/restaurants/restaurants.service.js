9214e0014887d70e1bd1b76ff649bf5bd026ce98
Ariel Vazquez <50372661+Arielvazquez@users.noreply.github.com>
2022-02-18 18:10:36 -0300

    Checkpoint camelize

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,4 +1,5 @@
 import { mocks } from "./mock";
+import camelize from "camelize";
 
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
@@ -9,9 +10,15 @@ export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
     resolve(mock);
   });
 };
+
+const restaurantsTransform = (result) => {
+  const newResult = camelize(result);
+  return newResult;
+};
 restaurantsRequest()
-  .then((result) => {
-    console.log(result);
+  .then(restaurantsTransform)
+  .then((transformedResponse) => {
+    console.log(transformedResponse);
   })
   .catch((err) => {
     console.log("error: " + err);
