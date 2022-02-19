40b8490d223f8cf689cf75f0837482c2cee020f5
Ariel Vazquez <50372661+Arielvazquez@users.noreply.github.com>
2022-02-18 22:03:57 -0300

    Checkpoint location context

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -30,7 +30,7 @@ const LoadingContainer = styled.View`
 
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
-  console.log(error);
+
   return (
     <SafeArea>
       {isLoading && (
