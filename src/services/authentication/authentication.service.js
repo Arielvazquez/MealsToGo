6cc253badceff3580f435ed9402ff654829a3909
Ariel Vazquez <50372661+Arielvazquez@users.noreply.github.com>
2022-02-23 18:11:07 -0300

    Checkpoint login screen exercise

--- src/services/authentication/authentication.service.js
+++ src/services/authentication/authentication.service.js
@@ -1,6 +1,6 @@
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-const loginRequest = (email, password) => {
+export const loginRequest = (email, password) => {
   const auth = getAuth();
   signInWithEmailAndPassword(auth, email, password);
 };
