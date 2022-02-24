6a3beb1bbb69987605fc6bc09e7c240d7723f9d7
Ariel Vazquez <50372661+Arielvazquez@users.noreply.github.com>
2022-02-24 18:41:47 -0300

    Checkpoint Env Utils

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,5 +1,5 @@
 const liveHost = "https://us-central1-mealstogo-f96cc.cloudfunctions.net";
-const localHost = " http://localhost:5001/mealstogo-f96cc/us-central1";
+const localHost = "http://localhost:5001/mealstogo-f96cc/us-central1";
 
 export const isDevelopment = process.env.NODE_ENV === "development";
 
